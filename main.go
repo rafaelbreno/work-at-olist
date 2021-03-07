ec6a7d7d3690611ac532b964d5e15e390538644d
Rafael Breno <rafael_breno@outlook.com>
2021-03-07 17:01:17 -0300

    Abstract config setting from main package

--- main.go
+++ main.go
@@ -1,21 +1,11 @@
 package main
 
 import (
-	"github.com/joho/godotenv"
+	_ "github.com/rafaelbreno/work-at-olist/cmd/config"
 	_ "github.com/rafaelbreno/work-at-olist/cmd/logger"
 	_ "github.com/rafaelbreno/work-at-olist/routes"
 )
 
-// Set environment
-func setEnv() {
-	err := godotenv.Load()
-
-	// TODO: Add error treatment
-	if err != nil {
-		panic(err)
-	}
-}
-
 func main() {
-	setEnv()
+
 }
