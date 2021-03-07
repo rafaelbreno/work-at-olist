585f227e9571d6402b28ead56b598a31220d7ad3
Rafael Breno <rafael_breno@outlook.com>
2021-03-06 21:06:45 -0300

    Add method to import env variables

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "github.com/joho/godotenv"
 
+// Set environment
+func setEnv() {
+	err := godotenv.Load()
+
+	// TODO: Add error treatment
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	setEnv()
 }
