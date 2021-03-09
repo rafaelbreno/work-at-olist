e35b5474fc7a16439eaad1828a015ec77ee5c210
Rafael Breno <rafael_breno@outlook.com>
2021-03-08 22:50:57 -0300

    Update Book's domain and DTO

--- domain/book.go
+++ domain/book.go
@@ -7,22 +7,17 @@ import (
 
 type Book struct {
 	gorm.Model
-	Name            string   `gorm:"name"`
-	Edition         uint     `gorm:"edition"`
-	PublicationYear uint     `gorm:"publication_year"`
-	Authors         []Author `gorm:"authors"`
+	Name            string `gorm:"name"`
+	Edition         uint   `gorm:"edition"`
+	PublicationYear uint   `gorm:"publication_year"`
+	Authors         []uint `gorm:"authors"`
 }
 
 func (b *Book) ToDTO() dto.BookResponse {
-	var authorsDTO []dto.AuthorResponse
-	for _, val := range b.Authors {
-		authorsDTO = append(authorsDTO, val.ToDTO())
-	}
-
 	return dto.BookResponse{
 		Name:            b.Name,
 		Edition:         b.Edition,
 		PublicationYear: b.PublicationYear,
-		Authors:         authorsDTO,
+		Authors:         b.Authors,
 	}
 }
--- dto/bookResponse.go
+++ dto/bookResponse.go
@@ -1,9 +1,9 @@
 package dto
 
 type BookResponse struct {
-	ID              uint             `json:"id"`
-	Name            string           `json:"name"`
-	Edition         uint             `json:"edition"`
-	PublicationYear uint             `json:"publication_year"`
-	Authors         []AuthorResponse `json:"authors"`
+	ID              uint   `json:"id"`
+	Name            string `json:"name"`
+	Edition         uint   `json:"edition"`
+	PublicationYear uint   `json:"publication_year"`
+	Authors         []uint `json:"authors"`
 }
